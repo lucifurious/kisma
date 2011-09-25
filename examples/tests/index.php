10f1f23139e280ba6e62dad8b984adaf502b6561
Jerry Ablan <jerryablan@gmail.com>
2011-09-25 01:12:15 -0400

    Local update/check-in

--- examples/tests/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined( 'YII_DEBUG' ) or define( 'YII_DEBUG', true );
-defined( 'YII_TRACE_LEVEL' ) or define( 'YII_TRACE_LEVEL', 3 );
-
-require_once( '/usr/local/yii/framework/yii.php' );
-
-$_config = require_once( dirname( __FILE__ ) . '/protected/config/main.php' );
-
-Yii::createWebApplication( $_config )->run();
