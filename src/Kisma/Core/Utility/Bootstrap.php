1b274b7015b398e8d10323953686bfb9dac2d4ea
Jerry Ablan <jerryablan@dreamfactory.com>
2012-10-10 09:35:11 -0400

    Cleanup

--- src/Kisma/Core/Utility/Bootstrap.php
+++ src/Kisma/Core/Utility/Bootstrap.php
@@ -9,7 +9,7 @@ use Kisma\Core\Interfaces\InputTypes;
  * Bootstrap class
  * Provides Twitter Bootstrap form-building
  */
-class Bootstrap implements \Kisma\Core\Interfaces\FormTypes
+class Bootstrap extends \Kisma\Core\Seed implements \Kisma\Core\Interfaces\FormTypes
 {
 	//*************************************************************************
 	//* Private Members
@@ -18,107 +18,43 @@ class Bootstrap implements \Kisma\Core\Interfaces\FormTypes
 	/**
 	 * @var string
 	 */
-	protected static $_formType = null;
+	protected $_formType = null;
+	/**
+	 * @var string If set, pre-pended to id and name attributes (id=prefix_field,name=prefix[field])
+	 */
+	protected $_prefix = null;
+	/**
+	 * @var string The HTML of the form
+	 */
+	protected $_contents = null;
 
 	//*************************************************************************
 	//* Public Methods
 	//*************************************************************************
 
 	/**
+	 * @param array|null|object|string $formType
+	 * @param string                   $prefix
+	 * @param array                    $settings
+	 */
+	public function __construct( $formType = self::Vertical, $prefix = null, $settings = array() )
+	{
+		$this->_formType = $formType;
+		$this->_prefix = $prefix;
+		$this->_contents = null;
+
+		parent::__construct( $settings );
+	}
+
+	/**
 	 * @param string $type
-	 * @param string $legend
 	 * @param array  $attributes
 	 *
 	 * @return string
 	 */
-	public static function startForm( $type = self::Vertical, $legend = null, array $attributes = array() )
+	public function input( $type, array $attributes = array() )
 	{
-		if ( is_array( $type ) )
-		{
-			$attributes = $type;
-			$type = self::Vertical;
-		}
-
-		if ( self::Vertical !== $type )
-		{
-			$_class = Markup::addValue( Option::get( $attributes, 'class', array() ), 'form' . $type );
-			$attributes['class'] = $_class;
-		}
-
-		self::$_formType = $type;
-		$_html = Markup::kvpToString( $attributes );
-
-		if ( !empty( $legend ) )
-		{
-			$legend = Markup::wrap( 'legend', $legend );
-		}
-
-		return <<<HTML
-<form {$_html}>
-	{$legend}
-HTML;
-
-	}
-
-	/**
-	 * @param string $formStart The form HTML up to this point...
-	 * @param bool   $submitButton
-	 *
-	 * @return string
-	 */
-	public static function endForm( $formStart, $submitButton = true )
-	{
-		$_submit = null;
-
-		if ( !empty( $submitButton ) )
-		{
-			$_submit = self::addButton(
-				array(
-					'type' => 'submit',
-					'text' => ( true === $submitButton ? 'Submit' : $submitButton ),
-				)
-			);
-
-		}
-
-		$_formStart = str_ireplace( '%%__BUTTONS__%%', $_submit, $formStart );
-
-		self::$_formType = null;
-
-		return <<<HTML
-{$_formStart}</form>
-HTML;
-
-	}
-
-	/**
-	 * @param array $attributes
-	 *
-	 * @return string
-	 */
-	public static function addButton( array $attributes = array() )
-	{
-		$_type = Option::get( $attributes, 'type', 'button', true );
-		$_class = Option::get( $attributes, 'class' );
-		$_text = Option::get( $attributes, 'text', 'Click Me!' );
-		$attributes['class'] = Markup::addValue( $_class, 'btn' );
-		$attributes = Markup::kvpToString( $attributes );
-
-		$_html = <<<HTML
-<button type="{$_type}" {$attributes}>{$_text}</button>
-HTML;
-
-		return $_html;
-	}
-
-	/**
-	 * @param array $attributes
-	 *
-	 * @return string
-	 */
-	public static function addInput( array $attributes = array() )
-	{
-		$_type = trim( strtolower( Option::get( $attributes, 'type', InputTypes::Text, true ) ) );
+		$_type = trim( strtolower( $type ) );
 		$_labelAttributes = Option::get( $attributes, 'labelAttributes', array(), true );
 		$_wrapInput = Scalar::in( $_type, 'checkbox', 'radio' );
 
@@ -132,6 +68,14 @@ HTML;
 			$_id = $_name;
 		}
 
+		if ( null !== $this->_prefix )
+		{
+			$_id = $this->_prefix . '_' . $_id;
+			$_name = $this->_prefix . '[' . $_name . ']';
+			$attributes['id'] = $_id;
+			$attributes['name'] = $_name;
+		}
+
 		if ( null !== ( $_label = Option::get( $attributes, 'label', null, true ) ) )
 		{
 			if ( null !== $_id && !isset( $_labelAttributes['for'] ) )
@@ -140,7 +84,7 @@ HTML;
 			}
 
 			//	Add .control-label for the class to labels
-			if ( self::Horizontal == self::$_formType )
+			if ( self::Horizontal == $this->_formType )
 			{
 				$_class = Option::get( $attributes, 'class', null );
 				$_labelAttributes['class'] = Markup::addValue( $_class, ( $_wrapInput ? $_type : 'control-label' ) );
@@ -158,7 +102,7 @@ HTML;
 		$_blockStart = $_blockEnd = null;
 		$_inputStart = $_inputEnd = null;
 
-		switch ( self::$_formType )
+		switch ( $this->_formType )
 		{
 			case self::Horizontal:
 				$_blockStart = Markup::tag( 'div', array( 'class'=> 'control-group' ), null, false );
@@ -175,7 +119,77 @@ HTML;
 		$attributes = Markup::kvpToString( $attributes );
 
 		$_html = <<<HTML
-{$_blockStart}{$_inputStart}{$_label}<input type="{$_type}" {$attributes}>{$_labelEnd}%%__BUTTONS__%%{$_hint}{$_inputEnd}{$_blockEnd}
+{$_blockStart}{$_inputStart}{$_label}<input type="{$_type}" {$attributes}>{$_labelEnd}{$_hint}{$_inputEnd}{$_blockEnd}
+HTML;
+
+		$this->_contents .= $_html;
+
+		return $_html;
+	}
+
+	/**
+	 * @param string $legend
+	 * @param string $submitButton
+	 * @param array  $attributes
+	 *
+	 * @internal param bool $submit
+	 * @return string
+	 */
+	public function renderForm( $legend = null, $submitButton = 'Submit', array $attributes = array() )
+	{
+		if ( self::Vertical !== $this->_formType )
+		{
+			$_class = Markup::addValue( Option::get( $attributes, 'class', array() ), 'form' . $this->_formType );
+			$attributes['class'] = $_class;
+		}
+
+		$_html = Markup::kvpToString( $attributes );
+
+		if ( !empty( $legend ) )
+		{
+			$legend = Markup::wrap( 'legend', $legend );
+		}
+
+		$_submit = null;
+		if ( !empty( $submitButton ) )
+		{
+			$_submit = $this->button(
+				'submit',
+				array(
+					'text' => ( true === $submitButton ? 'Submit' : $submitButton ),
+				)
+			);
+		}
+
+		$_html = <<<HTML
+<form {$_html}>
+	{$legend}
+	{$this->_contents}
+	{$_submit}
+</form>
+HTML;
+
+		$this->_contents = null;
+
+		return $_html;
+	}
+
+	/**
+	 * @param string $type
+	 * @param array  $attributes
+	 *
+	 * @return string
+	 */
+	public function button( $type = 'submit', array $attributes = array() )
+	{
+		$_type = trim( strtolower( $type ) ) ? : 'button';
+		$_class = Option::get( $attributes, 'class' );
+		$_text = Option::get( $attributes, 'text', 'Click Me!' );
+		$attributes['class'] = Markup::addValue( $_class, 'btn' );
+		$attributes = Markup::kvpToString( $attributes );
+
+		$_html = <<<HTML
+<button type="{$_type}" {$attributes}>{$_text}</button>
 HTML;
 
 		return $_html;
@@ -186,19 +200,63 @@ HTML;
 	//*************************************************************************
 
 	/**
-	 * @param $formType
+	 * @param string $contents
+	 *
+	 * @return Bootstrap
 	 */
-	public static function setFormType( $formType )
+	public function setContents( $contents )
 	{
-		self::$_formType = $formType;
+		$this->_contents = $contents;
+
+		return $this;
 	}
 
 	/**
-	 * @return null
+	 * @return string
 	 */
-	public static function getFormType()
+	public function getContents()
 	{
-		return self::$_formType;
+		return $this->_contents;
+	}
+
+	/**
+	 * @param string $formType
+	 *
+	 * @return Bootstrap
+	 */
+	public function setFormType( $formType )
+	{
+		$this->_formType = $formType;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFormType()
+	{
+		return $this->_formType;
+	}
+
+	/**
+	 * @param string $prefix
+	 *
+	 * @return Bootstrap
+	 */
+	public function setPrefix( $prefix )
+	{
+		$this->_prefix = $prefix;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPrefix()
+	{
+		return $this->_prefix;
 	}
 
 }
