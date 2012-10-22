3d8add164734334b9d2dcc8c6ab66feb4b661b68
Jerry Ablan <jerryablan@dreamfactory.com>
2012-10-22 10:12:11 -0400

    Checkpoint commit

---
+++ src/Kisma/Core/Enums/GlobFlags.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * GlobFlags.php
+ */
+namespace Kisma\Core\Enums;
+/**
+ * GlobFlags
+ * Ya know, for globbing...
+ */
+class GlobFlags extends SeedEnum implements \Kisma\Core\Interfaces\GlobFlags
+{
+}
