583c21c0d1b7921dffae6556852dcd328ce4e553
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-28 15:15:19 -0400

    rp

--- src/Kisma.php
+++ src/Kisma.php
@@ -38,7 +38,7 @@ class Kisma
     /**
      * @var string The current version
      */
-    const KISMA_VERSION = '0.2.37';
+    const KISMA_VERSION = '0.2.38';
     /**
      * @var string The current version
      * @deprecated Deprecated in 0.2.19, to be removed in 0.3.x
