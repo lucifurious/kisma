9476ebd0e575ac6e72f8be94cf3c2fadb3ebd993
Jerry Ablan <jerryablan@dreamfactory.com>
2014-07-29 15:06:05 -0400

    tweaked globbing code

--- src/Kisma/Core/Utility/FileSystem.php
+++ src/Kisma/Core/Utility/FileSystem.php
@@ -31,250 +31,264 @@ use Kisma\Core\Seed;
  */
 class FileSystem extends Seed implements UtilityLike
 {
-	//********************************************************************************
-	//* Members
-	//********************************************************************************
+    //********************************************************************************
+    //* Members
+    //********************************************************************************
 
-	/**
-	 * @var string The name of the current file
-	 */
-	protected $_fileName = false;
-	/**
-	 * @var \resource The handle of the current file
-	 */
-	protected $_fileHandle = false;
+    /**
+     * @var string The name of the current file
+     */
+    protected $_fileName = false;
+    /**
+     * @var \resource The handle of the current file
+     */
+    protected $_fileHandle = false;
 
-	//*************************************************************************
-	//* Methods
-	//*************************************************************************
+    //*************************************************************************
+    //* Methods
+    //*************************************************************************
 
-	/**
-	 * @param array|object $fileName
-	 *
-	 * @return \Kisma\Core\Utility\FileSystem
-	 */
-	public function __construct( $fileName )
-	{
-		parent::__construct();
+    /**
+     * @param array|object $fileName
+     *
+     * @return \Kisma\Core\Utility\FileSystem
+     */
+    public function __construct( $fileName )
+    {
+        parent::__construct();
 
-		$this->_fileName = $fileName;
-		$this->open();
-	}
+        $this->_fileName = $fileName;
+        $this->open();
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function validHandle()
-	{
-		return ( false !== $this->_fileHandle );
-	}
+    /**
+     * @return bool
+     */
+    public function validHandle()
+    {
+        return ( false !== $this->_fileHandle );
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function open()
-	{
-		if ( file_exists( $this->_fileName ) )
-		{
-			$this->_fileHandle = @fopen( $this->_fileName, 'r' );
-		}
+    /**
+     * @return bool
+     */
+    public function open()
+    {
+        if ( file_exists( $this->_fileName ) )
+        {
+            $this->_fileHandle = @fopen( $this->_fileName, 'r' );
+        }
 
-		return $this->validHandle();
-	}
+        return $this->validHandle();
+    }
 
-	/**
-	 * Close the file
-	 */
-	public function close()
-	{
-		if ( $this->_fileHandle )
-		{
-			@fclose( $this->_fileHandle );
-		}
+    /**
+     * Close the file
+     */
+    public function close()
+    {
+        if ( $this->_fileHandle )
+        {
+            @fclose( $this->_fileHandle );
+        }
 
-		$this->_fileHandle = false;
-	}
+        $this->_fileHandle = false;
+    }
 
-	/**
-	 * @return int|bool
-	 */
-	public function filesize()
-	{
-		return $this->validHandle() ? filesize( $this->_fileName ) : false;
-	}
+    /**
+     * @return int|bool
+     */
+    public function filesize()
+    {
+        return $this->validHandle() ? filesize( $this->_fileName ) : false;
+    }
 
-	/**
-	 * @return int|bool
-	 */
-	public function atime()
-	{
-		return $this->validHandle() ? fileatime( $this->_fileName ) : false;
-	}
+    /**
+     * @return int|bool
+     */
+    public function atime()
+    {
+        return $this->validHandle() ? fileatime( $this->_fileName ) : false;
+    }
 
-	/**
-	 * @return int|bool
-	 */
-	public function fileowner()
-	{
-		return $this->validHandle() ? fileowner( $this->_fileName ) : false;
-	}
+    /**
+     * @return int|bool
+     */
+    public function fileowner()
+    {
+        return $this->validHandle() ? fileowner( $this->_fileName ) : false;
+    }
 
-	/**
-	 * @return int|bool
-	 */
-	public function filegroup()
-	{
-		return $this->validHandle() ? filegroup( $this->_fileName ) : false;
-	}
+    /**
+     * @return int|bool
+     */
+    public function filegroup()
+    {
+        return $this->validHandle() ? filegroup( $this->_fileName ) : false;
+    }
 
-	/**
-	 * @param int $offset
-	 * @param int $whence
-	 *
-	 * @return int|bool
-	 */
-	public function fseek( $offset = 0, $whence = SEEK_SET )
-	{
-		return $this->validHandle() ? fseek( $this->_fileHandle, $offset, $whence ) : false;
-	}
+    /**
+     * @param int $offset
+     * @param int $whence
+     *
+     * @return int|bool
+     */
+    public function fseek( $offset = 0, $whence = SEEK_SET )
+    {
+        return $this->validHandle() ? fseek( $this->_fileHandle, $offset, $whence ) : false;
+    }
 
-	/**
-	 * @return int|bool
-	 */
-	public function ftell()
-	{
-		return $this->validHandle() ? ftell( $this->_fileHandle ) : false;
-	}
+    /**
+     * @return int|bool
+     */
+    public function ftell()
+    {
+        return $this->validHandle() ? ftell( $this->_fileHandle ) : false;
+    }
 
-	/**
-	 * Retrieves a string from the current file
-	 *
-	 * @param int $length
-	 *
-	 * @return string|bool
-	 */
-	public function fgets( $length = null )
-	{
-		return $this->validHandle() ? fgets( $this->_fileHandle, $length ) : false;
-	}
+    /**
+     * Retrieves a string from the current file
+     *
+     * @param int $length
+     *
+     * @return string|bool
+     */
+    public function fgets( $length = null )
+    {
+        return $this->validHandle() ? fgets( $this->_fileHandle, $length ) : false;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getFileName()
-	{
-		return $this->_fileName;
-	}
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->_fileName;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getFileHandle()
-	{
-		return $this->_fileHandle;
-	}
+    /**
+     * @return int
+     */
+    public function getFileHandle()
+    {
+        return $this->_fileHandle;
+    }
 
-	/**
-	 * Builds a path from arguments and validates existence.
-	 *
-	 * @param bool $validate    If true, will check path with is_dir.
-	 * @param bool $forceCreate If true, and result path doesn't exist, it will be created
-	 *
-	 * @throws \Kisma\Core\Exceptions\UtilityException
-	 * @return bool|null|string
-	 */
-	public static function makePath( $validate = true, $forceCreate = false )
-	{
-		$_arguments = func_get_args();
-		$_validate = $_path = null;
+    /**
+     * Builds a path from arguments and validates existence.
+     *
+     * @param bool $validate    If true, will check path with is_dir.
+     * @param bool $forceCreate If true, and result path doesn't exist, it will be created
+     *
+     * @throws \Kisma\Core\Exceptions\UtilityException
+     * @return bool|null|string
+     */
+    public static function makePath( $validate = true, $forceCreate = false )
+    {
+        $_arguments = func_get_args();
+        $_validate = $_path = null;
 
-		foreach ( $_arguments as $_part )
-		{
-			if ( is_bool( $_part ) )
-			{
-				$_validate = $_part;
-				continue;
-			}
+        foreach ( $_arguments as $_part )
+        {
+            if ( is_bool( $_part ) )
+            {
+                $_validate = $_part;
+                continue;
+            }
 
-			$_path .= DIRECTORY_SEPARATOR . trim( $_part, DIRECTORY_SEPARATOR . ' ' );
-		}
+            $_path .= DIRECTORY_SEPARATOR . trim( $_part, DIRECTORY_SEPARATOR . ' ' );
+        }
 
-		if ( !is_dir( $_path = realpath( $_path ) ) )
-		{
-			if ( $_validate && !$forceCreate )
-			{
-				return false;
-			}
+        if ( !is_dir( $_path = realpath( $_path ) ) )
+        {
+            if ( $_validate && !$forceCreate )
+            {
+                return false;
+            }
 
-			if ( $forceCreate )
-			{
-				if ( false === @mkdir( $_path, 0, true ) )
-				{
-					throw new UtilityException( 'The result path "' . $_path . '" could not be created.' );
-				}
-			}
-		}
+            if ( $forceCreate )
+            {
+                if ( false === @mkdir( $_path, 0, true ) )
+                {
+                    throw new UtilityException( 'The result path "' . $_path . '" could not be created.' );
+                }
+            }
+        }
 
-		return $_path;
-	}
+        return $_path;
+    }
 
-	/**
-	 * As found on php.net posted by: BigueNique at yahoo dot ca 20-Apr-2010 07:15
-	 * A safe empowered glob().
-	 *
-	 * Supported flags: GLOB_MARK, GLOB_NOSORT, GLOB_ONLYDIR
-	 * Additional flags: GlobFlags::GLOB_NODIR, GlobFlags::GLOB_PATH, GlobFlags::GLOB_NODOTS, GlobFlags::GLOB_RECURSE (not original glob() flags, defined here)
-	 *
-	 * @author BigueNique AT yahoo DOT ca
-	 *
-	 * @param string $pattern
-	 * @param int    $flags
-	 *
-	 * @return array|bool
-	 */
-	public static function glob( $pattern, $flags = 0 )
-	{
-		$_split = explode( '/', str_replace( '\\', '/', $pattern ) );
-		$_mask = array_pop( $_split );
-		$_path = implode( '/', $_split );
-		$_glob = false;
+    /**
+     * As found on php.net posted by: BigueNique at yahoo dot ca 20-Apr-2010 07:15
+     * A safe empowered glob().
+     *
+     * Supported flags: GLOB_MARK, GLOB_NOSORT, GLOB_ONLYDIR
+     * Additional flags: GlobFlags::GLOB_NODIR, GlobFlags::GLOB_PATH, GlobFlags::GLOB_NODOTS, GlobFlags::GLOB_RECURSE
+     * (not original glob() flags, defined here)
+     *
+     * @author BigueNique AT yahoo DOT ca
+     *
+     * @param string $pattern
+     * @param int    $flags
+     *
+     * @return array|bool
+     */
+    public static function glob( $pattern, $flags = 0 )
+    {
+        $_split = explode(
+            DIRECTORY_SEPARATOR,
+            str_replace( '\\', DIRECTORY_SEPARATOR, ltrim( $pattern, DIRECTORY_SEPARATOR ) )
+        );
 
-		if ( false !== ( $_directory = opendir( $_path ) ) )
-		{
-			$_glob = array();
+        $_mask = array_pop( $_split );
+        $_path = DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $_split );
 
-			while ( false !== ( $_file = readdir( $_directory ) ) )
-			{
-				//	Recurse directories
-				if ( ( $flags & GlobFlags::GLOB_RECURSE ) && is_dir( $_file ) && ( !in_array( $_file, array( '.', '..' ) ) ) )
-				{
-					$_glob = array_merge( $_glob,
-						Scalar::array_prepend( self::glob( $_path . '/' . $_file . '/' . $_mask,
-								$flags ),
-							( $flags & GlobFlags::GLOB_PATH ? '' : $_file . '/' ) ) );
-				}
+        $_glob = false;
 
-				// Match file mask
-				if ( fnmatch( $_mask, $_file ) )
-				{
-					if ( ( ( !( $flags & GLOB_ONLYDIR ) ) || is_dir( "$_path/$_file" ) ) &&
-						( ( !( $flags & GlobFlags::GLOB_NODIR ) ) || ( !is_dir( $_path . '/' . $_file ) ) ) &&
-						( ( !( $flags & GlobFlags::GLOB_NODOTS ) ) || ( !in_array( $_file, array( '.', '..' ) ) ) )
-					)
-					{
-						$_glob[] = ( $flags & GlobFlags::GLOB_PATH ? $_path . '/' : '' ) . $_file . ( $flags & GLOB_MARK ? '/' : '' );
-					}
-				}
-			}
+        if ( false !== ( $_directory = opendir( $_path ) ) )
+        {
+            $_glob = array();
 
-			closedir( $_directory );
+            while ( false !== ( $_file = readdir( $_directory ) ) )
+            {
+                $_fullPath = $_path . DIRECTORY_SEPARATOR . $_file;
 
-			if ( !( $flags & GLOB_NOSORT ) )
-			{
-				sort( $_glob );
-			}
-		}
+                //	Recurse directories
+                if ( is_dir( $_fullPath ) && ( $flags & GlobFlags::GLOB_RECURSE ) && !Scalar::in( $_file, '.', '..' ) )
+                {
+                    $_glob = array_merge(
+                        $_glob,
+                        Scalar::array_prepend(
+                            static::glob( $_fullPath . DIRECTORY_SEPARATOR . $_mask, $flags ),
+                            ( $flags & GlobFlags::GLOB_PATH ? null : $_file . DIRECTORY_SEPARATOR )
+                        )
+                    );
+                }
 
-		return $_glob;
-	}
+                // Match file mask
+                if ( fnmatch( $_mask, $_file ) )
+                {
+                    if ( ( ( !( $flags & GLOB_ONLYDIR ) ) || is_dir( $_fullPath ) ) &&
+                        ( ( !( $flags & GlobFlags::GLOB_NODIR ) ) || ( !is_dir( $_fullPath ) ) ) &&
+                        ( ( !( $flags & GlobFlags::GLOB_NODOTS ) ) || ( !in_array( $_file, array( '.', '..' ) ) ) )
+                    )
+                    {
+                        $_glob[] =
+                            ( $flags & GlobFlags::GLOB_PATH ? $_path . '/' : null ) .
+                            $_file .
+                            ( $flags & GLOB_MARK ? '/' : '' );
+                    }
+                }
+            }
+
+            closedir( $_directory );
+
+            if ( !empty( $_glob ) && !( $flags & GLOB_NOSORT ) )
+            {
+                sort( $_glob );
+            }
+        }
+
+        return $_glob;
+    }
 }
